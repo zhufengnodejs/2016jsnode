f55f77c9231f11f9e3bb2ecad0035798e39daa80
guoyongfeng <296512521@qq.com>
2016-06-05 15:52:14 +0800

    4.实现相加的功能

--- 3.jsonp/j.js
+++
@@ -1,4 +0,0 @@
-function say(){
-    console.log('c');
-}
-say();
--- 3.jsonp/jsonp.js
+++ 3.jsonp/jsonp.js
@@ -1,5 +1,15 @@
 var http = require('http');
 var fs = require('fs');
+var url = require('url');
 http.createServer(function(req,res){
-
-}).listen(9090);
+   var urlObj = url.parse(req.url,true);
+   var pathname = urlObj.pathname;//得到路径名
+   var query = urlObj.query;//得到查询字符串对象
+   if(pathname == '/calc'){
+       var result = parseInt(query.a) + parseInt(query.b);//得到a b并且执行相加
+       var cb = query.cb;
+       res.end(cb+'('+result+')');
+   }else{
+       res.end('404');
+   }
+}).listen(9090,'localhost');
