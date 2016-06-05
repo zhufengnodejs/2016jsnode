fa434fa0a00eef086315f1d5da27fd6e57812fb2
guoyongfeng <296512521@qq.com>
2016-06-05 11:25:24 +0800

    3.发送post请求

--- 2.ajax/2.ajaxpost.js
+++ 2.ajax/2.ajaxpost.js
@@ -2,6 +2,7 @@ var http = require('http');
 var fs = require('fs');
 //引入解析路径的URL模块
 var url = require('url');
+var querystring = require('querystring');
 http.createServer(function(request,response){
    //把URL从字符串转成对象
    var urlObj = url.parse(request.url);
@@ -11,8 +12,23 @@ http.createServer(function(request,response){
        //从文件创建一个可读流
         fs.createReadStream('./2.ajaxpost.html').pipe(response);// response是一个可写流
    }else if(pathname == '/ajax'){
-       response.statusCode = 404;
-       response.end('ajax');
+       var result = '';
+       //每当有请求体数据到来之后都会触发data事件
+       request.on('data',function(data){
+           result += data;
+       });
+       //请求体接收完毕之后会触发end事件
+       request.on('end',function(data){
+         // result = username=user&password=pwd
+         //我们希望把这个字符串转成对象
+         //注意 headers是个对象，它里面放的是所有的请求头，全部小写
+         var contentType =  request.headers['content-type'];
+         var body = {};//body是请求体对象
+         if(contentType == 'application/x-www-form-urlencoded'){
+            body = querystring.parse(result);
+         }
+          response.end(JSON.stringify(body));//把对象转成JSON字符串返回
+       });
    }else{
        response.end('404');
    }
