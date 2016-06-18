2e4256799f4f248d3d139921fc84a15bbe9fbec9
zhufengnodejs <zhufengnodejs@126.com>
2016-06-18 10:49:45 +0800

    mongoose

--- 12.mongodb/1.connect.js
+++ 12.mongodb/1.connect.js
@@ -1,12 +1,15 @@
 var mongoose = require('mongoose');//引入数据库操作模块
 //var db = mongoose.connect('mongodb://123.57.143.189:27017/201602js');//连接数据库
-var db = mongoose.connect('mongodb://localhost/201602js');//连接数据库
-db.connection.on('error',function(err){//连接失败触发error事件并执行回调
+//var db = mongoose.connect('mongodb://localhost/201602js');/
+
+mongoose.connect('mongodb://localhost/201602js');
+/*db.connection.on('error',function(err){//连接失败触发error事件并执行回调
     console.error(err);
-});
-db.connection.on('open',function(){//连接成功触发open事件并执行回调
+});*/
+
+/*db.connection.on('open',function(){//连接成功触发open事件并执行回调
     console.log('数据库连接成功');
-});
+});*/
 //定义schema 就是规定了集合中的文档拥有哪些字段，以及字些字段的类型是什么，默认值是什么
 var PersonSchema = new mongoose.Schema({
     name:{type:String},
@@ -16,7 +19,7 @@ var PersonSchema = new mongoose.Schema({
 });
 
 //定义一个模型 指定集合的名称和shema
-var personModel = db.model('person',PersonSchema);
+var personModel = mongoose.model('person',PersonSchema);
 
 var personEntity = new personModel({
     name:'张三',
