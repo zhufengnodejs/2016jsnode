42ce7f6621914bbc42692ca2bec7d05df8842e89
zhufengnodejs <zhufengnodejs@126.com>
2016-06-25 17:42:04 +0800

    绘制界面

---
+++ 17.chat/server.js
@@ -0,0 +1,8 @@
+var express = require('express');
+var path = require('path');
+var app = express();
+app.use(express.static(path.join(__dirname,'public')));
+app.get('/',function(req,res){
+    res.sendFile(path.resolve('./index.html'));
+});
+app.listen(9090);
