fb75d946aa151d18f3c84481db06764dcb26995b
zhufengnodejs <zhufengnodejs@126.com>
2016-06-25 12:13:54 +0800

    实现删除功能

--- 15.restful/restful.js
+++ 15.restful/restful.js
@@ -59,6 +59,31 @@ app.route('/users/:id').get(function(req,res){
             res.send(user);
         });
     });
+}).patch((req,res)=>{ //局部更新
+    var user = req.body;
+    readUsers(res, function (users) {
+        users = users.map(item => {
+            if(item.id == req.params.id){//如果当前用户跟URL中指定的用户ID相同的话则替换成新的用户，如果不相同则保持原数组不变
+                Object.assign(item,user);
+            }
+                return item;
+        })
+        saveUser(res, users, function () {
+            res.send(user);
+        });
+    });
+}).delete((req,res)=>{
+    readUsers(res, function (users) {
+        for(var i=0;i<users.length;i++){
+            if(users[i].id == req.params.id){
+                users.splice(i,1);
+                break;
+            }
+        }
+        saveUser(res, users, function () {
+            res.send({});//删除之后返回空对象
+        });
+    });
 
 })
 
