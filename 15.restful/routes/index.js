84d2fcd07c80070dea71eb763f63c2eeda43351d
zhufengnodejs <zhufengnodejs@126.com>
2016-06-25 12:53:11 +0800

    更加复杂的查询

--- 15.restful/routes/index.js
+++ 15.restful/routes/index.js
@@ -2,7 +2,8 @@ var fs = require('fs');
 module.exports = function(app){
   var files = fs.readdirSync(__dirname);
   files.forEach(function(file){
-      if(file != 'index.js')
-        require('./'+file)(app);
+      if(file != 'index.js'){
+          require('./'+file)(app);
+      }
   });
 }
--- 15.restful/routes/users.js
+++ 15.restful/routes/users.js
@@ -1,7 +1,18 @@
 var util = require('../util');
 module.exports = function(app){
+    // /users?keyword=1&pageNum=2&pageSize=2&orderBy=id&order=asc
     app.route('/users').get(function (req, res) { //查询所有用户
         util.readUsers(res, function (users) {
+            var keyword = req.query.keyword?req.query.keyword:'';
+            var orderBy = req.query.orderBy?req.query.orderBy:'id';
+            var order = req.query.order?req.query.order:'asc';
+            var query = new RegExp(keyword);// /a/
+            var pageNum = req.query.pageNum?Number(req.query.pageNum):1;
+            var pageSize = req.query.pageSize?Number(req.query.pageSize):2;
+            users = users.filter(item=>query.test(item.name)).sort((a,b)=>{
+                var asc = order == 'desc'?-1:1;
+                return (a[orderBy] - b[orderBy])*asc;
+            }).slice((pageNum-1)*pageSize,pageNum*pageSize);
             res.send(users);
         });
     }).post(function (req, res) { //新增加用户
