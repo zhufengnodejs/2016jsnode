a7d22dee7b11e8d680761600ac3e21ab1f3d3462
zhufengnodejs <zhufengnodejs@126.com>
2016-06-26 11:37:26 +0800

    路由

--- 17.chat/server.js
+++ 17.chat/server.js
@@ -9,9 +9,7 @@ var http = require('http');
 var server = http.createServer(app);
 var io = require('socket.io')(server);
 io.on('connection',function(socket){
-    console.log('客户端已经连接');
     socket.on('message',function(msg){
-        console.log(msg);
         io.emit('message',msg);
     });
 });
