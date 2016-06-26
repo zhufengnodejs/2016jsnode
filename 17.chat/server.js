335700ba9f609d7feeaf838bd33a894288527635
zhufengnodejs <zhufengnodejs@126.com>
2016-06-26 12:36:57 +0800

    聊天室

--- 17.chat/server.js
+++ 17.chat/server.js
@@ -8,9 +8,35 @@ app.get('/',function(req,res){
 var http = require('http');
 var server = http.createServer(app);
 var io = require('socket.io')(server);
+var rooms ={
+
+};
 io.on('connection',function(socket){
-    socket.on('message',function(msg){
-        io.emit('message',msg);
+    socket.on('addMessage',function(msgObj){
+        var roomId = msgObj.roomId;
+        var message = msgObj.message;
+        rooms[roomId].messages.push(message);
+        io.emit('messageAdded',message);
+    });
+    socket.on('getAllRooms',function(){
+        var arr = Object.keys(rooms).map(key=>{
+            return {id:key,name:rooms[key].name};
+        })
+        socket.emit('allRooms',arr);
+    });
+    socket.on('createRoom',function(roomName){
+        var id = Date.now()+'';
+        //创建新房间 id 是房间的ID value是房间名房间内的消息
+        rooms[id] = {
+            name:roomName,
+            messages:[]
+        }
+        io.emit('roomCreated',{id:id,name:roomName});
+    });
+
+    socket.on('getAllMessages',function(id){
+        var messages = rooms[id].messages;
+        socket.emit('allMessages',messages);
     });
 });
 
